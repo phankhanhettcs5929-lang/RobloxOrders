d0101f62735aca535810686ce14497e7b07a3aec
phankhanhettcs5929-lang <phankhanhettcs5929@gmail.com>
2026-05-27 23:19:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,3 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
